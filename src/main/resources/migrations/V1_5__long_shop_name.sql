ebb61de4af691658ce278dbc35220de36b515f8c
vadim <v.lipakov@simplex-software.ru>
2017-10-07 19:31:17 +0300

    #1012 Добавил цены вездов в отчет со страницы цен.

--- src/main/resources/migrations/V1_5__long_shop_name.sql
+++ src/main/resources/migrations/V1_5__long_shop_name.sql
@@ -1,2 +1,13 @@
 ALTER TABLE task
+   ALTER COLUMN name TYPE character varying(1024);
+ALTER TABLE task
    ALTER COLUMN shopname TYPE character varying(1024);
+ALTER TABLE task
+   ALTER COLUMN orderNumber TYPE character varying(1024);
+ALTER TABLE task
+   ALTER COLUMN wrikeLink TYPE character varying(1024);
+ALTER TABLE task
+   ALTER COLUMN path TYPE character varying(1024);
+
+ALTER TABLE city
+   ALTER COLUMN name TYPE character varying(1024);
